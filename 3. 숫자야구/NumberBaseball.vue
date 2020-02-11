080c1416c83328735c31178b57093aee252698c3
JKKorea <jongkun7987@gmail.com>
2020-02-11 23:56:42 +0900

    20200211

---
+++ 3. 숫자야구/NumberBaseball.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{result}}</h1>
+  <form v-on:submit="onSubmitForm">
+    <input ref="answer" maxlength="4" v-model="value"/>
+    <button>입력</button>
+  </form>
+  <div>시도: {{}}</div>
+</template>
+<script>
+  export default {
+    data() {
+        return {
+            value: '',
+            result: '',
+        }
+    },
+    methods: {
+      onSubmitFrom(e) {
+          e.preventDefault();
+      }
+    }
+  };
+</script>
+<style>
+
+</style>
--- 3. 숫자야구/webpack.config.js
+++ 3. 숫자야구/webpack.config.js
@@ -1,17 +1,24 @@
-module.exports = {
-	entry: {
-		app: './main.js',
-	},
-	module: { // module이 webpack의 핵심
-		rules: [{
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
 
-		}],
-	},
-	plugins: [],
-	output: {
-		// filename: 'app.js' // 종합해서 app.js라는 이름으로 파일을 합쳐줌.
-		filename: '[name].js', // 이렇게 해도 entry의 app을 보고 위와 같이 만들어줌.
-		path: './dist',
-	},
+module.exports = {
+  entry: {
+    app: path.join(__dirname, 'main.js'),
+  },
+  module: { // module이 webpack의 핵심
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader',
+
+    }],
+  },
+  plugins: [
+      new VueLoaderPlugin(),
+  ],
+  output: {
+    // filename: 'app.js' // 종합해서 app.js라는 이름으로 파일을 합쳐줌.
+    filename: '[name].js', // 이렇게 해도 entry의 app을 보고 위와 같이 만들어줌.
+    path: path.join(__dirname, 'dist'),
+  },
 
 }
